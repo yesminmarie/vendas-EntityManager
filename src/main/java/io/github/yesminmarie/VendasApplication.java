52dcb37f776b1fb6ef89cf84fac9aaec9b848527
Yesmin Lahoud <ymslahoud@gmail.com>
2022-11-07 09:53:01 -0300

    Criando operacoes de atualizar, deletar e buscarPorNome usando jdbcTemplate

--- src/main/java/io/github/yesminmarie/VendasApplication.java
+++ src/main/java/io/github/yesminmarie/VendasApplication.java
@@ -16,11 +16,36 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
+            System.out.println("Salvando clientes");
             clientes.salvar(new Cliente("Yesmin"));
             clientes.salvar(new Cliente("Outro cliente"));
 
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("cli").forEach(System.out::println);
+
+            System.out.println("Deletando clientes");
+            clientes.obterTodos().forEach(c -> {
+                clientes.deletar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado.");
+            } else {
+                todosClientes.forEach(System.out::println);
+            }
         };
     }
 
